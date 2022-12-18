aa03ead21284da07054e9d997c610c59c74fba74
Pablo <109902056+unstopabloz@users.noreply.github.com>
2022-12-18 01:07:20 +0100

    Added verbal code structure in the javascript file for the game

---
+++ game.js
@@ -0,0 +1,25 @@
+function getComputerChoice() {
+    // elige random number
+}
+
+function getPlayerChoice() {
+    // input por el choice
+}
+function gameRound() {
+    // funcion comparando inputs de computer y player para decidir a quien dar un punto con variables para un += y asi ir sumando
+}
+
+
+let ComputerChoice 
+let PlayerChoice 
+let computerScore = 0 
+let playerScore = 0 
+// creamos variables para los choices del bucle
+
+
+// creamos bucle para el juego hasta el primero llegar a 5
+while (computerScore < 5 || playerScore < 5) {
+    // bucle del juego
+}
+
+console.log("Game Over!")

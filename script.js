c7a33753aef44ce8a34ee0096a2d518b321cab2c
Pablo <109902056+unstopabloz@users.noreply.github.com>
2022-12-25 11:25:53 +0100

    Writen button logic and game-execution in progress

--- script.js
+++ script.js
@@ -16,91 +16,54 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let Playerchoise 
-    playerChoice = prompt("Choose between Rock, Paper or Scissors for this round: ")
-    playerChoice = playerChoice.toString()
-
-    while (playerChoice.length < 4 || playerChoice === false) {
-        playerChoice = prompt("Choose between Rock, Paper or Scissors for this round: ")
+function gameRound(button) {
+    let computerChoice = getComputerChoice();
+    let playerChoice;
+    if (button == 1) {
+        playerChoice = 'rock'
+    } else if (button == 2) {
+        playerChoice = 'paper'
+    } else if (button == 3) {
+        playerChoice = 'scissors'
     }
-
-    playerChoice = playerChoice.toLowerCase()
-    if (playerChoice == "rock") {
-        return "rock"
-    } else if (playerChoice == "paper") {
-        return "paper"
-    } else if (playerChoice == "scissors" || playerChoice == "scisors") {
-        return "scissors"
-    } else {
-        console.log("Rock chosen by default for not answering properly")
-        return "rock"
-    }
+    console.log(playerChoice);
 }
 
-function gameRound() {
-    ComputerChoice = getComputerChoice()
-    PlayerChoice = getPlayerChoice()
-
-    if (ComputerChoice == PlayerChoice) {
+function gameEngine(playerChoice, computerChoice) {
+    if (computerChoice == playerChoice) {
         console.log("This was a tie! No points ")
         computerPoints = 0
         playerPoints = 0
         return "nothing"
-    } else if (ComputerChoice == "rock" && PlayerChoice == "paper") {
+    } else if (computerChoice == "rock" && playerChoice == "paper") {
         console.log(`Player wins! Paper beats scissors`)
         computerPoints = 0
         playerPoints = 1
         return "player"
-    } else if (ComputerChoice == "rock" && PlayerChoice == "scissors") {
+    } else if (computerChoice == "rock" && playerChoice == "scissors") {
         console.log(`Computer wins! Rock beats scissors`)
         computerPoints = 1
         playerPoints = 0
         return "computer"
-    } else if (ComputerChoice == "paper" && PlayerChoice == "rock") {
+    } else if (computerChoice == "paper" && playerChoice == "rock") {
         console.log(`Computer wins! Paper beats rock`)
         computerPoints = 1
         playerPoints = 0
         return "computer"
-    } else if (ComputerChoice == "paper" && PlayerChoice == "scissors") {
+    } else if (computerChoice == "paper" && playerChoice == "scissors") {
         console.log(`Player wins! Scissors beats paper`)
         computerPoints = 0
         playerPoints = 1
         return "player"
-    } else if (ComputerChoice == "scissors" && PlayerChoice == "rock") {
+    } else if (computerChoice == "scissors" && playerChoice == "rock") {
         console.log(`Player wins! Rock beats scissors`)
         computerPoints = 0
         playerPoints = 1
         return "player"
-    } else if (ComputerChoice == "scissors" && PlayerChoice == "paper") {
+    } else if (computerChoice == "scissors" && playerChoice == "paper") {
         console.log(`Computer wins! Scissors beats paper`)
         computerPoints = 1
         playerPoints = 0
         return "computer"
     }
-}
-
-
-// Here the game starts // 
-let computerScore = 0 
-let playerScore = 0 
-let gameIncrease
-
-while (computerScore < 5 && playerScore < 5) {
-    console.log(`Game Score --> Player: ${playerScore} || Computer: ${computerScore} `)
-    gameIncrease = gameRound()
-
-    if (gameIncrease == "computer") {
-        computerScore += 1
-    } else if (gameIncrease == "player") {
-        playerScore += 1
-    } else if (gameIncrease == "nothing") {
-        console.log("No points increased!")
-    }
-}
-
-if (computerScore == 5) {
-    console.log("Game Over! Computer is victorious!")
-} else if (playerScore == 5) {
-    console.log("Game Over! Player is victorious!")
 }
